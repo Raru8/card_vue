e8ff686e85d77a3ea1f4163f23e198de9a9ab01c
Raru8 <victorgabriel8184@gmail.com>
2022-10-09 18:11:42 -0300

    Novos ajustes

---
+++ src/views/login/UserLogin.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="h-full w-full flex items-center justify-center">
+        <div
+            class="h-full w-1/2 flex flex-col items-center justify-center bg-[#00DFC2] text-[40px] text-center text-white font-light">
+            <img src="../../assets/images/bg-login.png">
+            <p class="mt-2">Star for free and get <br> attractive offers</p>
+        </div>
+        <div class="h-full w-1/2 flex flex-col items-center justify-center relative">
+            <h1 class="text-[50px] font-semibold absolute top-20 left-48">Login</h1>
+
+            <form class="h-96 flex flex-col justify-around">
+                <input-default :placeholder="'Email'" :label="'Login:'" :type="'text'" />
+                <input-default :placeholder="'Password'" :label="'Password:'" :type="'password'" />
+
+                <button-default :size="'h-10 w-96'" :title="'Login'" />
+
+                <span class="line-div">Or</span>
+
+                <button class="text-[#9B9B9B] text-base border-[1px] rounded-lg h-10 w-96 mt-7">
+                    <i class="fa-brands fa-google"></i>
+                    Sing in with Google
+                </button>
+
+            </form>
+
+        </div>
+    </div>
+</template>
+
+<script>
+import InputDefault from '@/components/forms/InputDefault';
+import ButtonDefault from '@/components/forms/ButtonDefault';
+
+export default {
+    name: 'UserLogin',
+    components: {
+        InputDefault,
+        ButtonDefault
+    }
+}
+</script>
+
+<style>
+.line-div {
+    top: 25px;
+    position: relative;
+    width: 24rem;
+    text-align: center;
+}
+
+.line-div::after,
+.line-div::before {
+    content: "";
+    position: absolute;
+    width: 45%;
+    height: 1px;
+    background-color: #00DFC2;
+}
+
+.line-div::after {
+    left: 0;
+    top: 50%;
+}
+
+.line-div::before {
+    right: 0;
+    top: 50%;
+}
+</style>
