5f3466293aa0f05d42c65365a0e1a0ef601dfca4
Raru8 <victorgabriel8184@gmail.com>
2022-10-09 18:11:18 -0300

    Novo componente InputDefault

---
+++ src/components/forms/InputDefault.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex justify-evenly flex-col h-20">
+        <label class="font-bold text-sm">{{label}}</label>
+        <input 
+        class="h-10 w-96 rounded-lg border-[1px] border-[#D9D9D9] pl-2 outline-none" 
+        :type="type"
+        :placeholder="placeholder">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InputDefault',
+    props: {
+        type: String,
+        placeholder: String,
+        label: String
+    }
+}
+</script>
