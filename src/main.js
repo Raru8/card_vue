8c49e1df6ede1d4424d8c411c50dd79ae637d20f
Raru8 <victorgabriel8184@gmail.com>
2022-10-11 21:20:58 -0300

    Importação do Pinia para main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import '@/assets/style.css';
-
+import { createPinia } from 'pinia'
 import FontAwesomeIcon from '@/components/utilities/FontAwesome'
+
+const pinia = createPinia()
 
 createApp(App)
     .component("FontAwesomeIcon", FontAwesomeIcon)
+    .use(pinia)
     .mount('#app')
