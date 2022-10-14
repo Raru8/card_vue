8ea41b6f88a438dde1812daac0b8e93b70b132b4
Raru8 <victorgabriel8184@gmail.com>
2022-10-13 23:02:40 -0300

    Iniciando Home

--- src/views/UserHome.vue
+++ src/views/UserHome.vue
@@ -1,3 +1,39 @@
 <template>
     <h1>Home</h1>
+    {{this.data}}
 </template>
+
+<script>
+import { stores } from '@/store'
+
+export default {
+    name: "UserHome",
+    props: {
+        id: String
+    },
+    data() {
+        return {
+            data: []
+        }
+    },
+    setup() {
+        const store = stores()
+        return { store }
+    },
+    methods: {
+        loadUser(id) {
+            this.store.loadUser(id).then((r) => {
+                this.data = r.data
+                console.log(r.data)
+            })
+        }
+
+    },
+    computed() {
+        return this.$router.params.id
+    },
+    mounted() {
+        this.loadUser(this.$route.params.id)
+    }
+}
+</script>
