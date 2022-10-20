384b079904fda7d43b921f37a59f505cd7c88560
Raru8 <victorgabriel8184@gmail.com>
2022-10-19 23:05:48 -0300

    Novos ajustes de position

--- src/components/nav/NavMenu.vue
+++ src/components/nav/NavMenu.vue
@@ -1,7 +1,7 @@
 <template>
     <nav
-        class="h-full w-20 relative float-left border-r-2 border-[#D9D9D9] flex flex-col justify-start items-center pt-20 text-[30px]">
-        <div class="h-1/2 flex flex-col justify-between mb-16">
+        class="h-full w-20 absolute float-left border-r-2 border-[#D9D9D9] flex flex-col justify-start items-center pt-20 text-[30px]">
+        <div class="h-1/2 flex flex-col justify-between items-center mb-16">
 
             <div class="hover:transition ease-in-out hover:drop-shadow-lg hover:text-[#00DFC2]">
                 <i class="fa-solid fa-house"></i>
